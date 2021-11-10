4a20f3a4070d3f077ffa38cb36cc4359e5793090
pjoiner <pjoiner@airmail.net>
2021-11-09 19:16:55 -0600

    Created LambdaMapper

---
+++ src/DwC-A_dotnet.Mapping/LambdaMapper.cs
@@ -0,0 +1,25 @@
+﻿extern alias Core;
+
+using Core.DwC_A;
+using System;
+
+namespace DwC_A.Mapping
+{
+    public class LambdaMapper<T> : IMapper<T> where T : new()
+    {
+        private readonly Action<T, IRow> rowMapFunc;
+
+        public LambdaMapper(Action<T, IRow> rowMapFunc)
+        {
+            this.rowMapFunc = rowMapFunc;
+        }
+
+        public T MapRow(IRow row)
+        {
+            T obj = new T();
+            rowMapFunc?.Invoke(obj, row);
+            return obj;
+        }
+
+    }
+}
---
+++ src/DwC-A_dotnet.Mapping/MapperExtensions.cs
@@ -0,0 +1,32 @@
+﻿extern alias Core;
+
+using Core.DwC_A;
+using System.Collections.Generic;
+
+namespace DwC_A.Mapping
+{
+    public static class MapperExtensions
+    {
+        public static T Map<T>(this IRow row, IMapper<T> mapper) where T : class, new()
+        {
+            return mapper.MapRow(row);
+        }
+
+        public static IEnumerable<T> Map<T>(this IEnumerable<IRow> rows, IMapper<T> mapper) where T : class, new()
+        {
+            foreach(var row in rows)
+            {
+                yield return mapper.MapRow(row);
+            }
+        }
+
+        public static IEnumerable<T> Map<T>(this IFileReader fileReader, IMapper<T> mapper) where T : class, new()
+        {
+            foreach(var row in fileReader.DataRows)
+            {
+                yield return mapper.MapRow(row);
+            }
+        }
+
+    }
+}
