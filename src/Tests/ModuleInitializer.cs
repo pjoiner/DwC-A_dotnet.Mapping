e56049b5b56b4920dfd382aa1f0311b51eb9d04c
Paul Joiner <pjoiner@bwh.harvard.edu>
2025-02-10 16:21:36 -0600

    Added unit test for source generator

---
+++ src/Tests/ModuleInitializer.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Tests
+{
+    public static class ModuleInitializer
+    {
+        [ModuleInitializer]
+        public static void Init()
+        {
+            VerifySourceGenerators.Initialize();
+        }
+    }
+}
---
+++ src/Tests/SourceGeneratorTest.cs
@@ -0,0 +1,28 @@
+﻿using DwC_A.Mapping;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace Tests
+{
+    public class SourceGeneratorTest
+    {
+        [Fact]
+        public async Task ShouldGenerateMapMethod()
+        {
+            var testSource = File.ReadAllText("./TestSourceFiles/Multimedia.cs");
+            SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(testSource);
+
+            CSharpCompilation compilation = CSharpCompilation.Create(
+                assemblyName: "Tests",
+                syntaxTrees: [syntaxTree]);
+
+            var generator = new MappingSourceGenerator();
+
+            GeneratorDriver driver = CSharpGeneratorDriver.Create(generator);
+
+            driver = driver.RunGenerators(compilation);
+
+            await Verify(driver);
+        }
+    }
+}
